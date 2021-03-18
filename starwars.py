d8135ca1a83deea68c5526762abdea34105328d8
borbelybalint <bbalint0404@gmail.com>
2021-03-18 10:29:32 +0100

    :sparkles:Adds moving meteor

--- starwars.py
+++ starwars.py
@@ -1,4 +1,5 @@
 import turtle
+from random import randint
 
 
 # funkciók majd ide
@@ -18,19 +19,36 @@ def le():
     ship.sety(ship.ycor()-20)
 
 
+def move_meteor():
+    if meteor.xcor() < -400:
+        meteor.setx(400)
+        meteor.sety(randint(-280, 280))
+    meteor.setx(meteor.xcor()-0.2)
+
+
 space = turtle.Screen()
 space.setup(width=800, height=600)
 space.bgpic("images/space.png")
 space.addshape("images/sprite.gif")
+space.addshape("images/meteor2.gif")
+
 space.onkeypress(balra, "Left")
 space.onkeypress(jobbra, "Right")
 space.onkeypress(fel, "Up")
 space.onkeypress(le, "Down")
+
 space.listen()
 
 ship = turtle.Turtle()
 ship.shape("images/sprite.gif")
 
-while True:
+meteor = turtle.Turtle()
+meteor.shape("images/meteor2.gif")
+meteor.penup()
+space.tracer(0)
+meteor.setx(400)
+space.tracer()
 
+while True:
+    move_meteor()
     space.update()
