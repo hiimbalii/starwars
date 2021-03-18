c3b0135dc9a782556934ceec2d61f1d66bd6f73e
hiimbalii <tcrbt1@gmail.com>
2021-03-18 10:03:11 +0100

    :sparkles: Adds mozgás

--- starwars.py
+++ starwars.py
@@ -2,6 +2,20 @@ import turtle
 
 
 # funkciók majd ide
+def balra():
+    ship.setx(ship.xcor()-20)
+
+
+def jobbra():
+    ship.setx(ship.xcor()+20)
+
+
+def fel():
+    ship.sety(ship.ycor()+20)
+
+
+def le():
+    ship.sety(ship.ycor()-20)
 
 
 space = turtle.Screen()
@@ -9,6 +23,10 @@ space.setup(width=800, height=600)
 space.bgpic("images/space.png")
 space.addshape("images/sprite.gif")
 space.onkeypress(balra, "Left")
+space.onkeypress(jobbra, "Right")
+space.onkeypress(fel, "Up")
+space.onkeypress(le, "Down")
+space.listen()
 
 ship = turtle.Turtle()
 ship.shape("images/sprite.gif")
